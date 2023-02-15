bac27a80be9c564a014aa1c3248d8088d5e31f92
Joao Lambert <devlambertjoao@gmail.com>
2023-02-15 02:17:54 -0300

    Script

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ npm install --prefix lsp/angularls
 npm install --prefix lsp/vscode
 npm install --prefix lsp/typescript
 
-unzip -uo lsp/clangd/linux-x64.zip -d lsp/clangd/linux-x64
-unzip -uo lsp/clangd/macos-arm.zip -d lsp/clangd/macos-arm
+rm -r lsp/clangd/linux-x64
+rm -r lsp/clangd/macos-arm
+rm -r lsp/clangd/windows
+
+unzip -uo lsp/clangd/linux-x64.zip -d lsp/clangd/
+unzip -uo lsp/clangd/macos-arm.zip -d lsp/clangd/
 
