b6903d74e00bb9ec59a4402368c2d7cdcbe86fe4
Joao Lambert <joao.lambert@gft.com>
2023-03-20 10:42:11 -0300

    Adding todo

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -2,6 +2,7 @@ return {
   'xiyaowong/transparent.nvim',
   event = { 'BufRead' },
   config = function()
+    -- TODO: Add transparency to telescope, and sign column "at left"
     require('transparent').setup({})
     vim.cmd[[ TransparentEnable ]]
   end
