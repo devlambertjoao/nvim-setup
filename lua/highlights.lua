29ca2420240f5c4bc05ad9a35c4a72af38d994d8
Joao Lambert <devlambertjoao@gmail.com>
2024-06-24 22:40:36 -0300

    Material palenight

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -6,22 +6,22 @@ vim.cmd([[
 ]])
 
 local palette = {
-	base00 = "#232c31",
-	base01 = "#1c3657",
-	base02 = "#2a343a",
-	base03 = "#3f4944",
-	base04 = "#84898c",
-	base05 = "#9ea7a6",
-	base06 = "#a7cfa3",
-	base07 = "#b5d8f6",
-	base08 = "#2a5491",
-	base09 = "#43820d",
-	base0A = "#a03b1e",
-	base0B = "#237986",
-	base0C = "#b02f30",
-	base0D = "#484d79",
-	base0E = "#c59820",
-	base0F = "#c98344",
+	base00 = "#292D3E",
+	base01 = "#444267",
+	base02 = "#32374D",
+	base03 = "#676E95",
+	base04 = "#8796B0",
+	base05 = "#959DCB",
+	base06 = "#959DCB",
+	base07 = "#FFFFFF",
+	base08 = "#F07178",
+	base09 = "#F78C6C",
+	base0A = "#FFCB6B",
+	base0B = "#C3E88D",
+	base0C = "#89DDFF",
+	base0D = "#82AAFF",
+	base0E = "#C792EA",
+	base0F = "#FF5370",
 }
 
 local hi = function(group, args)
