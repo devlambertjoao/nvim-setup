cd388e495a473c23fc32c8e767826d21996eff22
Joao Lambert <devlambertjoao@gmail.com>
2024-12-02 13:13:42 -0300

    nvim tree

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,5 +4,8 @@ return {
   cmd = 'Oil',
   config = function ()
     require('oil').setup({})
+
+		vim.keymap.set("n", "<leader>fo", "<cmd>Oil<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>fO", "<cmd>Oil .<CR>", { noremap = true, silent = true })
   end
 }
